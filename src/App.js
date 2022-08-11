b2c8a395c57e94e71536db051e4d41f91a19d8a5
sidesshmore <mosiddhesh@gmail.com>
2022-08-11 20:05:57 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
-// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react'
 import Alert from './components/Alert';
+// import About from './components/About';
+// import { Routes, Route } from 'react-router-dom';
+
 
 
 
@@ -13,7 +15,7 @@ function App() {
   const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
 
-  const showAlert = (message,type)=>{
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
       type: type
@@ -23,13 +25,13 @@ function App() {
     }, 1500);
   }
 
-  const toggleMode = () =>{
-    if(mode==='light'){
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#343a40';
       showAlert("Dark mode has been enabled", "success");
     }
-    else{
+    else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
@@ -38,11 +40,11 @@ function App() {
   }
   return (
     <>
-      <Navbar title="Textly" aboutText="About Text Utils" mode={mode} toggleMode={toggleMode}/>
-      <Alert alert={alert}/>
+      <Navbar title="Textly" aboutText="About Text Utils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <div className="container my-3">
+        <TextForm showAlert={showAlert} heading="Enter your text " mode={mode} />
         {/* <About/> */}
-        <TextForm showAlert={showAlert} heading="Enter your text " mode={mode}/>
       </div>
     </>
   );
